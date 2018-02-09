5d1a947fd652c290c2ab01a778e15fda18c9d2ed
David Frappart <david@frappart.com>
2018-02-09 18:02:26 +0100

    removing conditional in subnet module

--- Modules/06 Subnet/Subnet.tf
+++ Modules/06 Subnet/Subnet.tf
@@ -1,5 +1,5 @@
 ##############################################################
-#This module allows the creation of a Netsork Security Group
+#This module allows the creation of a Subnet
 ##############################################################
 
 #Variable declaration for Module
@@ -27,15 +27,10 @@ variable "Subnetaddressprefix" {
 
 variable "NSGid" {
   type    = "string"
-  default = "default"
+
 
 }
 
-variable "SubnetisGW" {
-  type    = "string"
-  default = "0"
-
-}
 
 variable "EnvironmentTag" {
   type    = "string"
@@ -51,9 +46,8 @@ variable "EnvironmentUsageTag" {
 
 resource "azurerm_subnet" "TerraSubnet" {
 
-#Use conditional to select if the Subnet is a Gateway subnet and should have a NSG
-#If SubnetisGW exist, count is set to 0 for this resource which is a subnet with NSG
-    count                       = "${var.SubnetisGW ? 0 : 1}"
+
+
     name                        = "${var.SubnetName}"
     resource_group_name         = "${var.RGName}"
     virtual_network_name        = "${var.vNetName}"
@@ -64,19 +58,6 @@ resource "azurerm_subnet" "TerraSubnet" {
 }
 
 
-resource "azurerm_subnet" "TerraSubnetGW" {
-
-#Use conditional to select if the Subnet is a Gateway subnet and should have a NSG
-#If SubnetisGW exist, count is set to 1 for this resource which is a subnet without NSG
-    count                       = "${var.SubnetisGW ? 1 : 0}"
-    name                        = "${var.SubnetName}"
-    resource_group_name         = "${var.RGName}"
-    virtual_network_name        = "${var.vNetName}"
-    address_prefix              = "${var.Subnetaddressprefix}"
-    #network_security_group_id   = "${var.NSGid}"
-
-
-}
 
 #Output
 
@@ -84,15 +65,15 @@ resource "azurerm_subnet" "TerraSubnetGW" {
 
 output "Name" {
 
-  value = "${var.SubnetisGW == "0" ? azurerm_subnet.TerraSubnet.name[0] : azurerm_subnet.TerraSubnetGW.name[0]}"
+  value = "${azurerm_subnet.TerraSubnet.name}"
 }
 
 output "Id" {
 
-  value = "${var.SubnetisGW == "0"? azurerm_subnet.TerraSubnet.id[0] : azurerm_subnet.TerraSubnetGW.id[0]}"
+  value = "${azurerm_subnet.TerraSubnet.id}"
 }
 
 output "AddressPrefix" {
 
-  value = "${var.SubnetisGW == "0" ? azurerm_subnet.TerraSubnet.address_prefix[0] : azurerm_subnet.TerraSubnetGW.address_prefix[0]}"
+  value = "${azurerm_subnet.TerraSubnet.address_prefix}"
 }
