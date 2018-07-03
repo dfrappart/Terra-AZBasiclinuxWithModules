adae2d6de1e6040833ef6b8883009b2ec7001cdf
dfrappart <david@frappart.com>
2018-07-03 11:52:11 +0200

    Adding Name as output to storage account container

--- Modules/04 StorageAccountContainer/StorageAccountContainer.tf
+++ Modules/04 StorageAccountContainer/StorageAccountContainer.tf
@@ -61,3 +61,7 @@ output "Properties" {
 output "RGName" {
   value = "${azurerm_storage_container.Terra-STC.resource_group_name}"
 }
+
+output "Name" {
+  value = "${azurerm_storage_container.name}"
+}
