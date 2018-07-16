4d945f1950d525f3fd58f07c30cd1a646c8a22e9
dfrappart <david@frappart.com>
2018-07-16 10:32:00 +0200

    Changing Modules naming

---
+++ Modules/21 LinuxVMAccess/21 LinuxVMAccess.tf
@@ -0,0 +1,56 @@
+###################################################################################
+#This module allows the creation of VMAccess Agent for linux
+###################################################################################
+
+#Variable declaration for Module
+
+variable "AgentCount" {
+  type    = "string"
+  default = "1"
+}
+
+variable "AgentName" {
+  type = "string"
+}
+
+variable "AgentLocation" {
+  type = "string"
+}
+
+variable "AgentRG" {
+  type = "string"
+}
+
+variable "VMName" {
+  type = "list"
+}
+
+variable "EnvironmentTag" {
+  type = "string"
+}
+
+variable "EnvironmentUsageTag" {
+  type = "string"
+}
+
+resource "azurerm_virtual_machine_extension" "Terra-CustomScriptLinuxAgent" {
+  count                = "${var.AgentCount}"
+  name                 = "${var.AgentName}${count.index+1}"
+  location             = "${var.AgentLocation}"
+  resource_group_name  = "${var.AgentRG}"
+  virtual_machine_name = "${element(var.VMName,count.index)}"
+  publisher            = "Microsoft.OSTCExtensions"
+  type                 = "VMAccessForLinux"
+  type_handler_version = "1.4"
+
+  settings = <<SETTINGS
+        {   
+        "commandToExecute": ""
+        }
+SETTINGS
+
+  tags {
+    environment = "${var.EnvironmentTag}"
+    usage       = "${var.EnvironmentUsageTag}"
+  }
+}
---
+++ Modules/22 CustomLinuxExtensionScript-Nginx/22 CustomLinuxExtensionScript-Nginx.tf
@@ -0,0 +1,62 @@
+###################################################################################
+#This module allows the creation of n CustomLinuxExtension and install nginx
+###################################################################################
+
+#Variable declaration for Module
+
+variable "AgentCount" {
+  type    = "string"
+  default = "1"
+}
+
+variable "AgentName" {
+  type = "string"
+}
+
+variable "AgentLocation" {
+  type = "string"
+}
+
+variable "AgentRG" {
+  type = "string"
+}
+
+variable "VMName" {
+  type = "list"
+}
+
+variable "EnvironmentTag" {
+  type = "string"
+}
+
+variable "EnvironmentUsageTag" {
+  type = "string"
+}
+
+resource "azurerm_virtual_machine_extension" "Terra-CustomScriptLinuxAgent" {
+  count                = "${var.AgentCount}"
+  name                 = "${var.AgentName}${count.index+1}"
+  location             = "${var.AgentLocation}"
+  resource_group_name  = "${var.AgentRG}"
+  virtual_machine_name = "${element(var.VMName,count.index)}"
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+        {   
+        
+        "fileUris": [ "https://raw.githubusercontent.com/dfrappart/Terra-AZBasiclinuxWithModules/master/Scripts/installnginx.sh" ],
+        "commandToExecute": "bash installnginx.sh"
+        }
+SETTINGS
+
+  tags {
+    environment = "${var.EnvironmentTag}"
+    usage       = "${var.EnvironmentUsageTag}"
+  }
+}
+
+output "RGName" {
+  value = "${var.AgentRG}"
+}
